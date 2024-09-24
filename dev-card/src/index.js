08d1929f90d15e7d10fae633d9915f5fef41dd9d
Momcilo Nikolic <momcilo.pos@gmail.com>
2024-09-24 22:21:52 +0200

    created adiditioner componenes

--- dev-card/src/index.js
+++ dev-card/src/index.js
@@ -7,7 +7,36 @@ import "./index.css";
 
 // app component
 
-function App() {}
+function App() {
+  return (
+    // card component
+    <div className="card">
+      {/* avatar photo */}
+      <Avatar/> 
+      {/* data */}
+      <div className="data">
+        {/* intro */}
+        <Intro/>
+        {/* dev skill list */}
+        <skillList/>
+      </div>
+    </div>
+  )
+}
+
+// aditioner components
+
+// Avatar
+function Avatar() {}
+
+// Intro 
+function Intro() {}
+
+// Skill List
+function SkillList() {}
+
+// skill
+function Skill() {}
 
 // render root method
 const root = ReactDOM.createRoot(document.querySelector("#root"));
